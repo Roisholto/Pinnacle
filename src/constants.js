d0f4051002041187f5cbac0224b359c9071c4524
Roisholto <roisoetan@gmail.com>
2020-05-18 15:58:16 +0100

    Add AUTH_URL to env

--- src/constants.js
+++ src/constants.js
@@ -10,13 +10,13 @@ export const SOCKET_ADDRESS = process.env.VUE_APP_SOCKET_ADDRESS
 
 export const MODE = process.env.NODE_ENV
 
-export const AUTH_URL = process.env.NODE_ENV == 'development' ? `http://localhost:8083/#/` : location.origin + '/auth/#/'
+export const AUTH_URL = process.env.NODE_ENV == 'development' ? `http://localhost:8083/#/` : process.env.VUE_APP_AUTH_URL 
 
 export const APP_NAME = process.env.VUE_APP_APP_NAME
 
 export const INSTALL_MODE = 'standalone' ;
 
-export const PRIMARY_COLOR = process.env.VUE_APP_PRIMARY_COLOR ? process.env.VUE_APP_PRIMARY_COLOR : '#f33348' 
+export const PRIMARY_COLOR = process.env.VUE_APP_PRIMARY_COLOR ? process.env.VUE_APP_PRIMARY_COLOR : '#f33348'
 
 // export const API_ENDPOINT = process
 export function calc_preorder_duration_unit (duration) {
