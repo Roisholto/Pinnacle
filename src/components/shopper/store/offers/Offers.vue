fd16a9bc6de9be51572db54fa7b9895a09b2df45
Roisholto <roisoetan@gmail.com>
2020-05-29 20:47:13 +0100

    created

---
+++ src/components/shopper/store/offers/Offers.vue
@@ -0,0 +1,29 @@
+<template>
+  <storeLayout>
+    <v-sheet class="d-flex justify-center flex-column py-3">
+      <div class="pb-3 text-center">
+        <v-icon color="primary" class="animate__animated animate__tada" size="62">
+          mdi-emoticon-sad-outline
+        </v-icon>
+      </div>
+      <div class="body-1 text-center">
+        Oops! No offers available, Check out later.
+      </div>
+    </v-sheet>
+  </storeLayout>
+</template>
+<script>
+import storeLayout from '@/layouts/storeLayout.vue' ;
+
+export default {
+  name:"Offers",
+
+  data(){
+    return {}
+  },
+
+  components:{
+    storeLayout
+  }
+}
+</script>
---
+++ src/views/OpeningHours.vue
@@ -0,0 +1,31 @@
+<template>
+  <storeLayout>
+    <v-card max-width="600" class="mx-auto" elevation="3">
+      <v-card-text>
+        <openingHours />
+      </v-card-text>
+    </v-card>
+  </storeLayout>
+</template>
+<script>
+import storeLayout from '@/layouts/storeLayout.vue'
+import openingHours from '@/components/shopper/store/profile/opening-hours.vue' ;
+
+export default {
+  name:"Page",
+
+  data(){
+    return {}
+  },
+
+  components:{
+    storeLayout,
+    openingHours
+  },
+
+
+  methods:{
+
+  }
+}
+</script>
