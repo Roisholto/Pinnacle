eebe862f3a021a7793174624454ae9db518145d0
Roisholto <roisoetan@gmail.com>
2020-05-29 20:50:32 +0100

    updated

--- src/components/shopper/store/top-banner.vue
+++ src/components/shopper/store/top-banner.vue
@@ -1,5 +1,5 @@
 <template>
-  <ImageSlider :name="'home'" :mid="mid" />
+  <ImageSlider :name="'home'" :mid="mid" :carousel="{showArrows:false}"/>
 </template>
 <script>
 import ImageSlider from './PageComponents/ImageSlider.vue' ;
