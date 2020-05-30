3e2ea17ea6ce2344d8f1ab76794d016bdef4cc74
Roisholto <roisoetan@gmail.com>
2020-05-30 05:28:27 +0100

    updated

--- src/components/shopper/store/single-product-category.vue
+++ src/components/shopper/store/single-product-category.vue
@@ -1,23 +1,22 @@
 <template>
+  <v-hover v-slot:default="{ hover }">
     <v-card class="position-relative justify-content-end align-center"
-    v-on:click="$emit('click')"
-    :flat="mode=='carousel'"
-    :tile="mode=='carousel'"
-    style="min-height:100px;">
-        <div class="image-container--16-9"
-            style="padding-top:70%">
-            <img v-bind:src="bg_image" />
-        </div>
-        <!--<v-card-title>
+      v-on:click="$emit('click')"
+      :flat="mode=='carousel'"
+      :tile="mode=='carousel'"
+      style="min-height:100px;">
+      <div
+        class="image-container--16-9"
+        style="padding-top:70%">
+        <img v-bind:src="bg_image" />
+      </div>
 
-        </v-card-title>-->
-        <v-card-text>
-        <!--<div class="card-body pa-2 w-100 text-capitalize d-flex flex-column justify-space-between" style="background: rgba(162, 158, 158, 0.2)">-->
-            <div class="body-2 font-weight-medium text-truncate text-capitalize">{{category.name}}</div>
-            <div class="caption primary--text text-right" v-show="mode!='carousel'">{{category.count}}</div>
-        <!--</div>-->
+      <v-card-text>
+        <div class="body-2 font-weight-medium text-truncate text-capitalize">{{category.name}}</div>
+        <div class="caption primary--text text-right" v-show="mode!='carousel'">{{category.count}}</div>
       </v-card-text>
     </v-card>
+  </v-hover>
 </template>
 <script>
 import { publicPath } from '@/constants.js'
