6bc566bfa117b8f2afbd3457522005380a92e225
Roisholto <roisoetan@gmail.com>
2020-06-14 21:45:59 +0100

    add APP_ASSETS_VERSION to env

--- vue.config.js
+++ vue.config.js
@@ -9,11 +9,12 @@ module.exports = {
   outputDir:path.resolve(__dirname, "shopper"),
   publicPath: process.env.PUBLIC_PATH,
  pwa:{
+   assetsVersion:process.env.APP_ASSETS_VERSION ? process.env.APP_ASSETS_VERSION : '',
    themeColor:'#f33348',
    iconPaths:{
-     appleTouchIcon: 'img/icons/apple-icon-152x152.png',
-     // maskIcon: 'img/icons/safari-pinned-tab.svg',
-     msTileImage: 'img/icons/ms-icon-144x144.png'
+    appleTouchIcon: 'img/icons/apple-icon-152x152.png',
+    msTileImage: 'img/icons/ms-icon-144x144.png',
+    maskIcon: 'img/icons/safari-pinned-tab.svg',
    },
    manifestOptions:{
     theme_color:process.env.VUE_APP_PRIMARY_COLOR,
