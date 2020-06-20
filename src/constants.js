9346e157a5d13dc89f87b0dc8dddceb3b747501d
Roisholto <roisoetan@gmail.com>
2020-06-20 18:54:46 +0100

    added PROMOTION_ENDPOINT

--- src/constants.js
+++ src/constants.js
@@ -10,7 +10,9 @@ export const SOCKET_ADDRESS = process.env.VUE_APP_SOCKET_ADDRESS
 
 export const MODE = process.env.NODE_ENV
 
-export const AUTH_URL = process.env.NODE_ENV == 'development' ? `http://localhost:8083/#/` : process.env.VUE_APP_AUTH_URL 
+export const AUTH_URL = process.env.NODE_ENV == 'development' ? `http://localhost:8083/#/` : process.env.VUE_APP_AUTH_URL
+
+export const PROMOTION_SERVICE_ENDPOINT = process.env.NODE_ENV == 'development' ? 'http://localhost:5000/api' : procee.env.VUE_APP_PROMOTION_ENDPOINT
 
 export const APP_NAME = process.env.VUE_APP_APP_NAME
 
