a13644e235deec624261a32ad17c78601ad5391b
Roisholto <roisoetan@gmail.com>
2020-07-11 19:00:39 +0100

    created

---
+++ src/components/shopper/RouteLoadingOverlay.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-dialog
+     v-model="dialog"
+     persistent
+     width="300"
+   >
+     <v-card
+       color="primary"
+       dark
+     >
+       <v-card-text class="text-center">
+         Please stand by
+         <v-progress-linear
+           indeterminate
+           color="white"
+           class="mb-0"
+         ></v-progress-linear>
+       </v-card-text>
+     </v-card>
+   </v-dialog>
+ </template>
+ <script>
+
+ export default{
+   name:"RouteLoadingOverlay",
+
+   computed:{
+     dialog:{
+       get(){
+         return this.$store.state.ui.loadingRoute ;
+       },
+
+       set(x){
+         this.$store.commit('ui/setLoadingRoute', x)
+       }
+     }
+   }
+ }
+ </script>
